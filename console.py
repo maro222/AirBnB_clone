299e3505fc58b9d9a61f5f8eee956aae0fbc28f6
Emil dawood <edawood997@gmail.com>
2023-10-14 14:21:18 +0300

    doneeeee

--- console.py
+++ console.py
@@ -55,7 +55,7 @@ class HBNBCommand(cmd.Cmd):
         elif (len(arr) == 2):
             if (arr[0] not in HBNBCommand.list_of_classes):
                 print("** class doesn't exist **")
-            for key, value in FileStorage().all().items():
+            for key, value in storage.all().items():
                 if f"{arr[0]}.{arr[1]}" == key:
                     print(value)
                     break
@@ -71,7 +71,7 @@ class HBNBCommand(cmd.Cmd):
             else:
                 print("** instance id missing **")
         else:
-            for key, value in FileStorage().all().items():
+            for key, value in storage.all().items():
                 isFound = False
                 if (f"{arr[0]}.{arr[1]}" == key):
                     del storage.all()[key]
@@ -84,7 +84,7 @@ class HBNBCommand(cmd.Cmd):
     def do_all(self, line):
         arr = line.split()
         emptyArr = []
-        for key, value in FileStorage().all().items():
+        for key, value in storage.all().items():
             emptyArr.append(str(value))
         if (len(arr) == 0):
             print(emptyArr)
@@ -114,10 +114,10 @@ class HBNBCommand(cmd.Cmd):
                 value_edit = int(arr[3])
             else:
                 value_edit = arr[3]
-                for key, value in FileStorage().all().items():
+                for key, value in storage.all().items():
                     if (f"{arr[0]}.{arr[1]}" == key):
                         setattr(value, arr[2], value_edit)
-                        FileStorage().save()
+                        storage.save()
                         break
 
     def do_count(self, line):
@@ -143,14 +143,14 @@ class HBNBCommand(cmd.Cmd):
             arr_of_line = line.split(".")
             if (arr_of_line[0] in HBNBCommand.list_of_classes):
                 if (arr_of_line[1] == "all()"):
-                    for key, value in FileStorage().all().items():
+                    for key, value in storage.all().items():
                         helper = key.split(".")
                         original_class = helper[0]
                         if (arr_of_line[0] == original_class):
                             emptyArr.append(str(value))
                     print(emptyArr)
                 elif (arr_of_line[1] == "count()"):
-                    for key, value in FileStorage().all().items():
+                    for key, value in storage.all().items():
                         helper = key.split(".")
                         original_class = helper[0]
                         if (arr_of_line[0] == original_class):
@@ -159,7 +159,7 @@ class HBNBCommand(cmd.Cmd):
                 elif (arr_of_line[1][0:4] == "show"):
                     is_found = False
                     spec_id = arr_of_line[1][6:-2]
-                    for key, value in FileStorage().all().items():
+                    for key, value in storage.all().items():
                         helper = key.split(".")
                         original_class = helper[0]
                         if (arr_of_line[0] == original_class):
@@ -172,7 +172,7 @@ class HBNBCommand(cmd.Cmd):
                 elif (arr_of_line[1][0:7] == "destory"):
                     is_found = False
                     spec_id = arr_of_line[1][9:-2]
-                    for key, value in FileStorage().all().items():
+                    for key, value in storage.all().items():
                         helper = key.split(".")
                         original_class = helper[0]
                         if (arr_of_line[0] == original_class):
@@ -188,7 +188,7 @@ class HBNBCommand(cmd.Cmd):
                     anoth_arr = arr_of_line[1].split(",", 1)
                     spec_id = anoth_arr[0][8:-1]
                     dic_for_update = eval(anoth_arr[1][1:-1])
-                    for key, value in FileStorage().all().items():
+                    for key, value in storage.all().items():
                         helper = key.split(".")
                         original_class = helper[0]
                         if (arr_of_line[0] == original_class):
@@ -206,7 +206,7 @@ class HBNBCommand(cmd.Cmd):
                     spec_id = anoth_arr[0][8:-1]
                     k = anoth_arr[1][2:-1]
                     v = anoth_arr[2][1:-1]
-                    for key, value in FileStorage().all().items():
+                    for key, value in storage.all().items():
                         helper = key.split(".")
                         original_class = helper[0]
                         if (arr_of_line[0] == original_class):
